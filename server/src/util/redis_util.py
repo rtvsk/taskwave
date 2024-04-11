2bf51eabfcc1ded2fa66b22fd2fbe40457ceb645
Valeriya <Dzhavadova.v@yandex.ru>
2024-04-11 20:37:08 +0300

    add caching function

--- server/src/util/redis_util.py
+++ server/src/util/redis_util.py
@@ -6,40 +6,6 @@ from functools import wraps
 from src.config import REDIS_HOST, REDIS_PORT
 
 
-# class RedisCache:
-#     def __init__(self):
-#         self.redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
-
-#     def set_cache(
-#         self, key: str, data: dict[str, Any], expire_time: Optional[int] = None
-#     ) -> None:
-#         """
-#         Cache data with a key
-#         """
-#         serialized_data = pickle.dumps(data)
-
-#         self.redis_client.set(key, serialized_data, ex=expire_time)
-
-#     def get_cache(self, key: str) -> Any:
-#         """
-#         Retrieve data from the cache for the given key
-#         """
-#         serialized_data = self.redis_client.get(key)
-
-#         if serialized_data is None:
-#             return None
-
-#         data = pickle.loads(serialized_data)
-
-#         return data
-
-#     def del_cache(self, key: str) -> None:
-#         """
-#         Delete data from the cache for the given key
-#         """
-#         self.redis_client.delete(key)
-
-
 class RedisCache:
 
     __REDIS_CLIENT = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
@@ -69,7 +35,8 @@ class RedisCache:
 
         return data
 
-    def del_cache(cls, key: str) -> None:
+    @classmethod
+    def delete_cache(cls, key: str) -> None:
         """
         Delete data from the cache for the given key
         """
@@ -80,7 +47,8 @@ def cache_data(key: str, expire_time: Optional[int] = None) -> Callable:
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         async def wrapper(*args, **kwargs):
-            cache_key = f"{key}:{kwargs}"
+            for v in kwargs.values():
+                cache_key = f"{key}:{v}"
 
             cached_data = RedisCache.get_cache(cache_key)
             if cached_data is not None:
@@ -89,23 +57,6 @@ def cache_data(key: str, expire_time: Optional[int] = None) -> Callable:
             response = await func(*args, **kwargs)
             if response:
                 RedisCache.set_cache(cache_key, response, expire_time=expire_time)
-
-            return response
-
-        return wrapper
-
-    return decorator
-
-
-def update_cache_data(key: str, expire_time: Optional[int] = None) -> Callable:
-    def decorator(func: Callable) -> Callable:
-        @wraps(func)
-        async def wrapper(*args, **kwargs):
-
-            response = await func(*args, **kwargs)
-            cache_key = f"{key}:{response.author_id}"
-
-            RedisCache.set_cache(cache_key, response, expire_time=expire_time)
 
             return response
 
