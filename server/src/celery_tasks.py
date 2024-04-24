02508afdf763077913fcc7cdba30562598752ee3
Valeriya <Dzhavadova.v@yandex.ru>
2024-04-24 12:47:02 +0300

    add func get_tasksgroup_from_db

--- server/src/celery_tasks.py
+++ server/src/celery_tasks.py
@@ -1,10 +1,11 @@
 import asyncio
 from sqlalchemy import select, and_
 
-# from datetime import date
+from datetime import datetime, UTC, timedelta
 from src.database import async_session_maker
 from src.celeryconfig import app
 from src.users.models import User
+from src.tasks_group.models import TasksGroup
 from src.util.email_util import Email
 
 
@@ -33,3 +34,31 @@ async def send_reminder_letter() -> None:
 def send_test_email() -> None:
     loop = asyncio.get_event_loop()
     loop.run_until_complete(send_reminder_letter())
+
+
+async def get_tasksgroup_from_db() -> list[TasksGroup]:
+    async with async_session_maker() as session:
+        result = await session.execute(
+            select(TasksGroup).where(
+                and_(
+                    TasksGroup.deadline == datetime.now(UTC).date() + timedelta(days=3),
+                    TasksGroup.is_done == False,
+                )
+            )
+        )
+        tasks_groups = result.fetchall()
+        print(tasks_groups)
+
+        return [tasks_group[0] for tasks_group in tasks_groups]
+
+
+# async def get_user_from_task(tasks) -> list[User]:
+#     user_bond_task = {}
+
+#     tasks_groups = user.tasks_groups
+#     deadlines = []
+#     for tasks_group in tasks_groups:
+#         tasks = tasks_group.tasks
+#         for task in tasks:
+#             if task.deadline:
+#                 deadlines.append(task.deadline)
