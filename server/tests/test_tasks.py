712b3d443f963442bd67fe1a36cecc29aa47d78a
Valeriya <Dzhavadova.v@yandex.ru>
2024-05-07 13:41:30 +0300

    func test_get_tasks

--- server/tests/test_tasks.py
+++ server/tests/test_tasks.py
@@ -29,3 +29,23 @@ async def test_create_task(
 
     for key, value in TASK_DATA.items():
         assert value == response_data[key]
+
+
+@pytest.mark.anyio
+async def test_get_tasks(
+    api_client: AsyncClient, created_test_access_token, get_entity_from_db
+):
+    tasks_group_from_db = await get_entity_from_db(
+        TasksGroup, "title", EDIT_TASKS_GROUP_DATA["title"]
+    )
+    headers = created_test_access_token
+
+    response = await api_client.get(
+        f"api/tasks/{tasks_group_from_db.id}", headers=headers
+    )
+    response.status_code == 200
+    response_data = response.json()
+
+    assert len(response_data) == 1
+    for key, value in TASK_DATA.items():
+        assert value == response_data[0][key]
