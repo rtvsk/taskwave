61169ef6bd3d8fe5217ec0248479123c07083673
Valeriya <Dzhavadova.v@yandex.ru>
2024-05-09 16:24:40 +0300

    add Celery task schedule

--- server/src/celeryconfig.py
+++ server/src/celeryconfig.py
@@ -1,12 +1,12 @@
-from datetime import timedelta
 from celery import Celery
+from celery.schedules import crontab
 
 app = Celery("tasks", broker="redis://localhost:6379/0")
 
 
 app.conf.beat_schedule = {
-    "send_test_email_2min": {
-        "task": "src.celery_tasks.send_test_letter",
-        "schedule": timedelta(minutes=2),
+    "send_reminder_email_every_day": {
+        "task": "src.celery_tasks.send_letter",
+        "schedule": crontab(minute=0, hour=0),
     },
 }
