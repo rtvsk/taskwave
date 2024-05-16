27c4ddfd4ba6e3bc6b74807fa1a1dceb6162fc7c
Valeriya <Dzhavadova.v@yandex.ru>
2024-05-16 15:20:49 +0300

    add redis settings

--- server/src/config.py
+++ server/src/config.py
@@ -1,3 +1,82 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+
+class DatabaseSettings(BaseSettings):
+    HOST: str
+    PORT: str
+    NAME: str
+    USER: str
+    PASSWORD: SecretStr
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore", env_prefix="DB_"
+    )
+
+    @property
+    def URL(self):
+        return SecretStr(
+            f"postgresql+asyncpg://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.NAME}"
+        )
+
+
+# class JWTSettings(BaseSettings):
+#     ACCESS_TOKEN_EXPIRE_MINUTES: int
+#     REFRESH_TOKEN_EXPIRE_DAYS: int
+#     SECRET_KEY: SecretStr
+#     ALGORITHM: str
+
+#     model_config = SettingsConfigDict(
+#         env_file=".env", env_file_encoding="utf-8", env_prefix="JWT_"
+#     )
+
+
+class SMTPSettings(BaseSettings):
+
+    EMAIL: str
+    PASSWORD: SecretStr
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", env_prefix="SMTP_", extra="ignore"
+    )
+
+
+class RedisSettings(BaseSettings):
+
+    HOST: str
+    PORT: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", env_prefix="REDIS_", extra="ignore"
+    )
+
+
+# class TestDatabaseSettings(BaseSettings):
+#     HOST: str
+#     PORT: str
+#     NAME: str
+#     USER: str
+#     PASSWORD: SecretStr
+
+#     model_config = SettingsConfigDict(
+#         env_file=".env", env_file_encoding="utf-8", env_prefix="TEST_DB_"
+#     )
+
+#     def URL(self):
+#         return SecretStr(
+#             f"postgresql+asyncpg://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.NAME}"
+#         )
+
+
+class Settings(BaseSettings):
+    smtp: SMTPSettings = SMTPSettings()
+    db: DatabaseSettings = DatabaseSettings()
+    #     jwt: JWTSettings = JWTSettings()
+    redis: RedisSettings = RedisSettings()
+
+
+settings = Settings()
+
 from dotenv import load_dotenv
 import os
 
@@ -8,22 +87,17 @@ DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
 
-DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
-DB_PORT_TEST = os.environ.get("DB_PORT_TEST")
-DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
-DB_USER_TEST = os.environ.get("DB_USER_TEST")
-DB_PASS_TEST = os.environ.get("DB_PASS_TEST")
-DATABASE_URL_TEST = f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASS_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}"
+TEST_DB_HOST = os.environ.get("TEST_DB_HOST")
+TEST_DB_PORT = os.environ.get("TEST_DB_PORT")
+TEST_DB_NAME = os.environ.get("TEST_DB_NAME")
+TEST_DB_USER = os.environ.get("TEST_DB_USER")
+TEST_DB_PASSWORD = os.environ.get("TEST_DB_PASSWORD")
+TEST_DATABASE_URL = f"postgresql+asyncpg://{TEST_DB_USER}:{TEST_DB_PASSWORD}@{TEST_DB_HOST}:{TEST_DB_PORT}/{TEST_DB_NAME}"
 
-ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
-SECRET_KEY = os.environ.get("SECRET_KEY")
-ALGORITHM = os.environ.get("ALGORITHM")
-
-REDIS_HOST = os.environ.get("REDIS_HOST")
-REDIS_PORT = os.environ.get("REDIS_PORT")
-
-SMTP_EMAIL = os.environ.get("SMTP_EMAIL")
-SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("JWT_ACCESS_TOKEN_EXPIRE_MINUTES")
+REFRESH_TOKEN_EXPIRE_DAYS = os.environ.get("JWT_REFRESH_TOKEN_EXPIRE_DAYS")
+SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+ALGORITHM = os.environ.get("JWT_ALGORITHM")
