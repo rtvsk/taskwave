ce1510cd7af32e7616996943790c9aaa2d6fc073
Valeriya <Dzhavadova.v@yandex.ru>
2024-05-16 13:27:53 +0300

    rename PASS to PASSWORD

--- server/migrations/env.py
+++ server/migrations/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASSWORD
 
 import os
 import sys
@@ -26,7 +26,7 @@ config.set_section_option(section, "DB_HOST", DB_HOST)
 config.set_section_option(section, "DB_PORT", DB_PORT)
 config.set_section_option(section, "DB_NAME", DB_NAME)
 config.set_section_option(section, "DB_USER", DB_USER)
-config.set_section_option(section, "DB_PASS", DB_PASS)
+config.set_section_option(section, "DB_PASSWORD", DB_PASSWORD)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- server/src/database.py
+++ server/src/database.py
@@ -2,10 +2,12 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from src.config import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
 
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = (
+    f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
