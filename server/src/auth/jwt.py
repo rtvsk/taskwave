c60d94854c34c87f423df75f87b227d4d283c992
Valeriya <Dzhavadova.v@yandex.ru>
2024-05-21 14:33:42 +0300

    create JwtToken

--- server/src/auth/jwt.py
+++ server/src/auth/jwt.py
@@ -19,3 +19,50 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
         algorithm=settings.jwt.ALGORITHM,
     )
     return encoded_jwt
+
+
+class JwtToken:
+
+    TOKEN_TYPE_FIELD: str = "type"
+    ACCESS_TOKEN_TYPE: str = "access"
+    REFRESH_TOKEN_TYPE: str = "refresh"
+
+    @staticmethod
+    def _encode(data: dict, expires_delta: timedelta) -> str:
+        to_encode = data.copy()
+        to_encode.update({"exp": expires_delta})
+        encoded = jwt.encode(
+            claims=to_encode,
+            key=settings.jwt.SECRET_KEY.get_secret_value(),
+            algorithm=settings.jwt.ALGORITHM,
+        )
+        return encoded
+
+    @classmethod
+    def decode(cls, token: str | bytes) -> dict:
+        decoded = jwt.decode(
+            token=token,
+            key=settings.jwt.SECRET_KEY.get_secret_value(),
+            algorithms=settings.jwt.ALGORITHM,
+        )
+        return decoded
+
+    @classmethod
+    def create_access_token(cls, data: dict) -> str:
+        payload = {cls.TOKEN_TYPE_FIELD: cls.ACCESS_TOKEN_TYPE}
+        payload.update(data)
+        expire = datetime.now(UTC) + timedelta(
+            minutes=settings.jwt.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+        encoded_access_jwt = cls._encode(data=payload, expires_delta=expire)
+        return encoded_access_jwt
+
+    @classmethod
+    def create_refresh_token(cls, data: dict) -> str:
+        payload = {cls.TOKEN_TYPE_FIELD: cls.REFRESH_TOKEN_TYPE}
+        payload.update(data)
+        expire = datetime.now(UTC) + timedelta(
+            days=settings.jwt.REFRESH_TOKEN_EXPIRE_DAYS
+        )
+        encoded_refresh_jwt = cls._encode(data=payload, expires_delta=expire)
+        return encoded_refresh_jwt
