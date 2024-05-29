170e390ea6e0c82da4b529ad3cc994fb604c660d
Valeriya <Dzhavadova.v@yandex.ru>
2024-05-29 16:50:24 +0300

    change class RedisCache

--- server/src/util/redis_util.py
+++ server/src/util/redis_util.py
@@ -11,57 +11,74 @@ logger = logging.getLogger(__name__)
 
 
 class RedisCache:
+    """
+    Service class for caching data.
+    """
 
-    __REDIS_CLIENT = redis.Redis(
-        host=settings.redis.HOST, port=settings.redis.PORT, db=0
-    )
+    def __init__(self, host: str, port: str, db: int):
+        """
+        Initialize Redis cache service.
 
-    @classmethod
-    def set_cache(
-        cls, key: str, data: dict[str, Any], expire_time: Optional[int] = None
+        :param host: Redis server host.
+        :param port: Redis server port.
+        :param db: Redis database number.
+        """
+        self.redis_client = redis.Redis(host=host, port=port, db=db)
+
+    def set(
+        self, key: str, data: dict[str, Any], expire_time: Optional[int] = None
     ) -> None:
         """
         Cache data with a key
         """
-        serialized_data = pickle.dumps(data)
+        try:
+            serialized_data = pickle.dumps(data)
+            self.redis_client.set(key, serialized_data, ex=expire_time)
+        except Exception as e:
+            logger.error(f"Error setting cache for key {key}: {e}")
 
-        cls.__REDIS_CLIENT.set(key, serialized_data, ex=expire_time)
-
-    @classmethod
-    def get_cache(cls, key: str) -> Any:
+    def get(self, key: str) -> Any:
         """
         Retrieve data from the cache for the given key
         """
-        serialized_data = cls.__REDIS_CLIENT.get(key)
+        try:
+            serialized_data = self.redis_client.get(key)
 
-        if serialized_data:
-            data = pickle.loads(serialized_data)
-            return data
+            if serialized_data:
+                data = pickle.loads(serialized_data)
+                return data
+        except Exception as e:
+            logger.error(f"Error getting cache for key {key}: {e}")
 
-    @classmethod
-    def delete_cache(cls, key: str) -> None:
+    def delete(self, key: str) -> None:
         """
         Delete data from the cache for the given key
         """
-        cls.__REDIS_CLIENT.delete(key)
+        try:
+            self.redis_client.delete(key)
+        except Exception as e:
+            logger.error(f"Error deleting cache for key {key}: {e}")
 
 
-def cache_data(key: str, expire_time: Optional[int] = None) -> Callable:
-    def decorator(func: Callable) -> Callable:
+redis_cache = RedisCache(host=settings.redis.HOST, port=settings.redis.PORT, db=0)
+
+
+def cache_data(key: str, expire_time: Optional[int] = None):
+    def decorator(func: Callable):
         @wraps(func)
         async def wrapper(*args, **kwargs):
             for v in kwargs.values():
                 cache_key = f"{key}:{v}"
 
-            cached_data = RedisCache.get_cache(cache_key)
+            cached_data = redis_cache.get(cache_key)
             if cached_data:
-                logger.debug(f"Cached data from Redis: {cached_data}")
+                logger.debug(f"Cached data from Redis with key: {cache_key}")
                 return cached_data
 
             response = await func(*args, **kwargs)
             if response:
-                RedisCache.set_cache(cache_key, response, expire_time=expire_time)
-                logger.debug(f"Sent data to Redis: {cache_key}")
+                redis_cache.set(cache_key, response, expire_time=expire_time)
+                logger.debug(f"Sent data to Redis for key: {cache_key}")
 
             return response
 
