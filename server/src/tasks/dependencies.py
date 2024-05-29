2159d025e8aac8fda561370ccdebdebc8a5c56a8
Valeriya <Dzhavadova.v@yandex.ru>
2024-05-29 12:09:50 +0300

    add docstring

--- server/src/tasks/dependencies.py
+++ server/src/tasks/dependencies.py
@@ -10,12 +10,20 @@ from src.database import get_async_session
 
 
 def get_task_service(session: AsyncSession = Depends(get_async_session)):
+    """
+    Provide a TaskService instance.
+    """
     return TaskService(session=session)
 
 
 async def valid_task_id(
     task_id: int, task_service: TaskService = Depends(get_task_service)
-):
+) -> Task:
+    """
+    Validate if a task with the given id exists.
+
+    If task doesn't exist, TaskNotFound error is raised.
+    """
     task = await task_service.get(task_id)
     if not task:
         raise TaskNotFound
@@ -27,6 +35,12 @@ async def valid_tasks_group(
     tasks_group: TasksGroup = Depends(valid_owned_tasks),
     task: Task = Depends(valid_task_id),
 ):
+    """
+    Validate if a task task belongs to a given tasks group.
+
+    If task doesn't belongs for the given tasks group,
+    TaskNotFound error is raised.
+    """
     if task.tasks_group_id != tasks_group.id:
         raise TaskNotFound
     return task
--- server/src/tasks/service.py
+++ server/src/tasks/service.py
@@ -1,15 +1,22 @@
 from uuid import UUID
+from typing import Optional
 
 from src.util.repository import BaseRepository
 from src.tasks.models import Task
 from src.tasks.schemas import CreateTask, UpdateTask
-from src.tasks.exceptions import TaskNotFound
 
 
 class TaskService(BaseRepository):
+    """
+    Service class for handling Task-related CRUD operations.
+    """
+
     model = Task
 
     async def create(self, task_data: CreateTask, tasks_group_id: UUID) -> Task:
+        """
+        Create a new task.
+        """
         task_dict = task_data.model_dump()
         task_dict["tasks_group_id"] = tasks_group_id
 
@@ -17,21 +24,33 @@ class TaskService(BaseRepository):
 
         return task
 
-    async def get(self, task_id: int) -> Task | None:
+    async def get(self, task_id: int) -> Optional[Task]:
+        """
+        Retrieve a task by id.
+        """
         task = await self.get_by_id(task_id)
 
         return task
 
-    async def get_from_tasks_group(self, tasks_group_id: UUID) -> list[Task] | None:
+    async def get_from_tasks_group(self, tasks_group_id: UUID) -> Optional[list[Task]]:
+        """
+        Retrieve all tasks belonging to the tasks group.
+        """
         tasks = await self.get_by_field("tasks_group_id", tasks_group_id, all=True)
 
         return tasks
 
     async def update_task(self, task_data: UpdateTask, task_id: int) -> Task:
+        """
+        Update a task.
+        """
         updated_task_params = task_data.model_dump(exclude_none=True)
         updated_task = await self.update("id", task_id, updated_task_params)
 
         return updated_task
 
     async def delete_task(self, task_id: int) -> None:
+        """
+        Delete a task by id.
+        """
         await self.delete(task_id)
