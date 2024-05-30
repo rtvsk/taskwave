3853df80631b075561adfbc638b072c2e6729cb8
Dmitry Ivanov <dmitry.ivanov@cherehapa.ru>
2024-05-30 19:50:15 +0300

    add dockerfile

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:react-hooks/recommended',
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    ignorePatterns: ['dist', 'nodeServer', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
     plugins: [
         'react',
