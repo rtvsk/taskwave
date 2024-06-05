caf58f35e98286365e76aa600ee49946a875d1f7
Dmitry Ivanov <dmitry.ivanov@cherehapa.ru>
2024-06-05 12:47:01 +0300

    rename client reminder to taskwave

--- client/nodeServer/nodeServer.js
+++ client/nodeServer/nodeServer.js
@@ -7,6 +7,10 @@ const PORT = 5173;
 
 app.use(express.static(path.join(__dirname, '..', 'dist')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
