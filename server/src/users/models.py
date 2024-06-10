6abbce0b1ae966c95665eefd24bfe8bfd1e302dc
Valeriya <Dzhavadova.v@yandex.ru>
2024-06-10 21:53:04 +0300

    add relationship

--- server/src/users/models.py
+++ server/src/users/models.py
@@ -2,12 +2,17 @@ import uuid
 
 from sqlalchemy import Column, String, Boolean
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 from src.database import Base
 
 
 class User(Base):
     __tablename__ = "user"
+
+    def __repr__(self) -> str:
+        return f"user-{self.login}"
+
     id = Column(
         UUID(as_uuid=True),
         primary_key=True,
@@ -22,3 +27,5 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     is_verified = Column(Boolean(), default=False)
     is_active = Column(Boolean(), default=True)
+
+    tasks_groups = relationship("TasksGroup", back_populates="author")
