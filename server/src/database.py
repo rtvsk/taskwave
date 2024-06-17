574f470b1932685b9d92eaeadd9d18d0d4551b22
Valeriya <Dzhavadova.v@yandex.ru>
2024-06-17 16:46:34 +0300

    add exception handling on connection

--- server/src/database.py
+++ server/src/database.py
@@ -1,9 +1,12 @@
+import logging
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 from src.config import settings
+from src.exceptions import DatabaseException
 
+logger = logging.getLogger(__name__)
 
 engine = create_async_engine(settings.db.URL.get_secret_value(), echo=True)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
@@ -14,5 +17,9 @@ class Base(DeclarativeBase):
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with async_session_maker() as session:
-        yield session
+    try:
+        async with async_session_maker() as session:
+            yield session
+    except OSError as e:
+        logger.error(f"Failed to connect to the database: {e}")
+        raise DatabaseException
