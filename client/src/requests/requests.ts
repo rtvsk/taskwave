686c81d2673c94dbfc8edd628658973fd6ccb1f9
Dmitry Ivanov <dmitry.ivanov@cherehapa.ru>
2024-06-19 22:04:56 +0300

    lasttry

--- client/src/requests/requests.ts
+++ client/src/requests/requests.ts
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 // Create an Axios instance with default configuration
 export const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
+    // baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'https://taskwave.ru',
 });
